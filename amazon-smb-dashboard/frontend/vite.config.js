46390d2ea5ae0268894516602ac0ffc5c023d16f
Hemanvitha Pullela <hemanvithapullela@gmail.com>
2024-11-21 18:39:46 +0530

    deploy

--- amazon-smb-dashboard/frontend/vite.config.js
+++ amazon-smb-dashboard/frontend/vite.config.js
@@ -4,12 +4,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', 
+    outDir: 'dist' // Ensure Vite builds into 'frontend/dist'
   },
-
-  base: process.env.BASE_URL || '/', 
+  base: '/', // Set base to '/' unless deploying to a subpath
   server: {
-    port: 3000, 
-    open: true, 
-  },
+    port: 3000,
+    open: true
+  }
 });
